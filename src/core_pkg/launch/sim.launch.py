d6d3aa96a35f96b780d55ee62f9a84b26a0c5af2
aky <akiyugata271@gmail.com>
2026-06-27 20:17:12 +0900

    Add launch argument for world name in simulation launch file

--- src/core_pkg/launch/sim.launch.py
+++ src/core_pkg/launch/sim.launch.py
@@ -3,6 +3,8 @@ import os
 from launch import LaunchDescription
 from launch.actions import AppendEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
@@ -11,11 +13,18 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
 
+    world_name = LaunchConfiguration('world_name')
+    DeclareLaunchArgument(
+        'world_name',
+        default_value='mtg_room.world',
+        description='Name of the world file to load'
+    )
+
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
     world = os.path.join(
         get_package_share_directory('sim_pkg'),
         'worlds',
-        'mtg_room.world'
+        world_name
     )
 
     set_env_vars_resources = AppendEnvironmentVariable(
@@ -32,5 +41,13 @@ def generate_launch_description():
         launch_arguments={'gz_args': ['-r -s -v2 ', world], 'on_exit_shutdown': 'true'}.items()
     )
     ld.add_action(gzserver_cmd)
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
+    )
+    # ld.add_action(gzclient_cmd)
 
     return ld
