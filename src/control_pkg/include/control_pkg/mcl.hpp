d61a676ae3f7eb03dbd8738d36a7baeba7798b25
aky <akiyugata271@gmail.com>
2026-07-01 18:58:52 +0900

    Implement MCL data structures and class definition in mcl.hpp and mcl.cpp

--- src/control_pkg/include/control_pkg/mcl.hpp
+++ src/control_pkg/include/control_pkg/mcl.hpp
@@ -1,1 +1,47 @@
 #pragma once
+
+#include <Eigen/Dense>
+
+struct DistanceFieldMap
+{
+    double resolution = 0.0;
+    double origin_x = 0.0;
+    double origin_y = 0.0;
+    double origin_yaw = 0.0;
+    int width = 0;
+    int height = 0;
+    Eigen::ArrayXXf distance;
+};
+
+struct Pose2D
+{
+    double x;
+    double y;
+    double theta;
+};
+
+struct particle
+{
+    Pose2D pose;
+    double weight;
+};
+
+struct mcl_parameters
+{
+    double dt;
+    double particle_num;
+    double sigma_x;
+    double sigma_y;
+    double sigma_theta;
+    DistanceFieldMap map;
+};
+
+
+class Mcl
+{
+public:
+    Mcl();
+
+private:
+    Pose2D now_pose_;
+};
---
+++ src/control_pkg/src/mcl.cpp
@@ -0,0 +1,6 @@
+#include "mcl.hpp"
+
+Mcl::Mcl()
+{
+
+}
